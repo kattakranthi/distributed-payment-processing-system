58faef6b106ca0722b2c70b5583402ef3502c540
kranthi katta <kattakranthi@gmail.com>
2026-05-25 16:51:22 -0400

    add in-memory payment repository

---
+++ src/main/java/com/example/paymentservice/entity/PaymentEntity.java
@@ -0,0 +1,44 @@
+package com.example.paymentservice.entity;
+
+import com.example.paymentservice.model.PaymentStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentEntity {
+
+    @Id
+    private String paymentId;
+
+    private BigDecimal amount;
+
+    private String currency;
+
+    private String payerId;
+
+    private String payeeId;
+
+    private String idempotencyKey;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private LocalDateTime createdAt;
+}
